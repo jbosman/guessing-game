c66c688797c744849ff0e66d45e7f80559aa981e
Joey <Joey@JOEYS-MACBOOK-PRO.local>
2016-07-04 15:29:24 -0600

    Disabled sumbit button functionality once the number of guesses hits zero.

--- guessing-game.js
+++ guessing-game.js
@@ -9,16 +9,25 @@ $(document).ready( function() {
 	// ************ SUBMIT GUESS BUTTON CLICK ************ //
 
 	$("form").find(".submit-guess").on("click", function() {	
+		// Prevent default button click actions
+		// Mainly this stops the page from refreshing
 		event.preventDefault();
 
-		var playerGuessDOM = $("#playerGuess");
+		// First check if there are guesses left
+		// if not make the button useless
+		if( gameState.getNumOfGuesses() > 0 ) {
+			var playerGuessDOM = $("#playerGuess");
 
-		//console.log("Submit Guess Button Pressed");
-		gameState.checkGuess(playerGuessDOM.val());
-		playerGuessDOM.val("");
+			//console.log("Submit Guess Button Pressed");
 
+			// Submit the players next guess
+			gameState.checkGuess(playerGuessDOM.val());
+			// Clear the current guess from the text field
+			playerGuessDOM.val("");
 
-		gameState.printGameState();
+			// Printing the game state for debugging
+			gameState.printGameState();
+		}
 	})
 
 	// ************ RESTART BUTTON CLICK ************ //
